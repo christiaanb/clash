8fcabd5ec3e690fe6c244e0ff0e6a80720a5ed11
Matthijs Kooijman <matthijs@stdin.nl>
2010-03-31 13:56:34 +0200

    Derive Show for CoreContext.

--- cλash/CLasH/Normalize/NormalizeTypes.hs
+++ cλash/CLasH/Normalize/NormalizeTypes.hs
@@ -29,6 +29,6 @@ data CoreContext = AppFirst        -- ^ The expression is the first
                  | LambdaBody      -- ^ The expression is the body of a
                                    --   lambda abstraction
                  | Other           -- ^ Another context
-  deriving Eq
+  deriving (Eq, Show)
 -- | Transforms a CoreExpr and keeps track if it has changed.
 type Transform = [CoreContext] -> CoreSyn.CoreExpr -> TransformMonad CoreSyn.CoreExpr
