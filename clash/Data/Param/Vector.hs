3dd66eb934352a894c7609a92a546c599fa05ea9
christiaanb <christiaan.baaij@gmail.com>
2010-10-25 17:06:54 +0200

    Prefix all vector functions with the character 'v'

--- clash/Data/Param/Vector.hs
+++ clash/Data/Param/Vector.hs
@@ -1,46 +1,46 @@
-{-# LANGUAGE StandaloneDeriving, ExistentialQuantification, ScopedTypeVariables, TemplateHaskell, TypeOperators, TypeFamilies #-}
+{-# LANGUAGE TemplateHaskell, ExistentialQuantification, TypeOperators, TypeFamilies, ScopedTypeVariables #-}
 module Data.Param.Vector
   ( Vector
   , empty
   , (+>)
   , singleton
-  , vectorTH
+  , vTH
   , unsafeVector
   , readVector
-  , length
-  , lengthT
+  , vlength
+  , vlengthT
   , fromVector
-  , null
+  , vnull
   , (!)
-  , replace
-  , head
-  , last
-  , init
-  , tail
-  , take
-  , drop
-  , select
+  , vreplace
+  , vhead
+  , vlast
+  , vinit
+  , vtail
+  , vtake
+  , vdrop
+  , vselect
   , (<+)
-  , (++)
-  , map
-  , zipWith
-  , foldl
-  , foldr
-  , zip
-  , unzip
-  , shiftIntoL
-  , shiftIntoR
-  , rotl
-  , rotr
-  , concat
-  , reverse
-  , iterate
-  , iteraten
-  , generate
-  , generaten
-  , copy
-  , copyn
-  , split
+  , (<++>)
+  , vmap
+  , vzipWith
+  , vfoldl
+  , vfoldr
+  , vzip
+  , vunzip
+  , (+>>)
+  , (<<+)
+  , vrotl
+  , vrotr
+  , vconcat
+  , vreverse
+  , viterate
+  , viteraten
+  , vgenerate
+  , vgeneraten
+  , vcopy
+  , vcopyn
+  , vsplit
   ) where
     
 import Types
@@ -49,19 +49,13 @@ import Types.Data.Num.Decimal.Literals.TH
 import Data.Param.Index
 
 import Data.Typeable
-import qualified Prelude as P
-import Prelude hiding (
-  null, length, head, tail, last, init, take, drop, (++), map, foldl, foldr,
-  zipWith, zip, unzip, concat, reverse, iterate )
-import qualified Data.Foldable as DF (Foldable, foldr)
-import qualified Data.Traversable as DT (Traversable(traverse))
+import qualified Data.Foldable as DF
+import qualified Data.Traversable as DT
 import Language.Haskell.TH hiding (Pred)
 import Language.Haskell.TH.Syntax (Lift(..))
 
 newtype (NaturalT s) => Vector s a = Vector {unVec :: [a]}
   deriving Eq
-
--- deriving instance (NaturalT s, Typeable s, Data s, Typeable a, Data a) => Data (TFVec s a)
 
 -- ==========================
 -- = Constructing functions =
@@ -79,16 +73,16 @@ singleton :: a -> Vector D1 a
 singleton x = x +> empty
 
 -- FIXME: Not the most elegant solution... but it works for now in clash
-vectorTH :: (Lift a) => [a] -> ExpQ
+vTH :: (Lift a) => [a] -> ExpQ
 -- vectorTH xs = sigE [| (TFVec xs) |] (decTFVecT (toInteger (P.length xs)) xs)
-vectorTH [] = [| empty |]
-vectorTH [x] = [| singleton x |]
-vectorTH (x:xs) = [| x +> $(vectorTH xs) |]
+vTH []     = [| empty |]
+vTH [x]    = [| singleton x |]
+vTH (x:xs) = [| x +> $(vTH xs) |]
 
 unsafeVector :: NaturalT s => s -> [a] -> Vector s a
 unsafeVector l xs
-  | fromIntegerT l /= P.length xs =
-    error (show 'unsafeVector P.++ ": dynamic/static lenght mismatch")
+  | fromIntegerT l /= length xs =
+    error (show 'unsafeVector ++ ": dynamic/static lenght mismatch")
   | otherwise = Vector xs
 
 readVector :: (Read a, NaturalT s) => String -> Vector s a
@@ -97,17 +91,17 @@ readVector = read
 -- =======================
 -- = Observing functions =
 -- =======================
-length :: forall s a . NaturalT s => Vector s a -> Int
-length _ = fromIntegerT (undefined :: s)
+vlength :: forall s a . NaturalT s => Vector s a -> Int
+vlength _ = fromIntegerT (undefined :: s)
 
-lengthT :: NaturalT s => Vector s a -> s
-lengthT = undefined
+vlengthT :: NaturalT s => Vector s a -> s
+vlengthT = undefined
 
 fromVector :: NaturalT s => Vector s a -> [a]
 fromVector (Vector xs) = xs
 
-null :: Vector D0 a -> Bool
-null _ = True
+vnull :: Vector D0 a -> Bool
+vnull _ = True
 
 (!) :: PositiveT s => Vector s a -> Index s -> a
 (Vector xs) ! i = xs !! (fromInteger (toInteger i))
@@ -115,121 +109,121 @@ null _ = True
 -- ==========================
 -- = Transforming functions =
 -- ==========================
-replace :: PositiveT s =>
+vreplace :: PositiveT s =>
   Vector s a -> Index s -> a -> Vector s a
-replace (Vector xs) i y = Vector $ replace' xs (toInteger i) y
+vreplace (Vector xs) i y = Vector $ replace' xs (toInteger i) y
   where replace' []     _ _ = []
         replace' (_:xs) 0 y = (y:xs)
         replace' (x:xs) n y = x : (replace' xs (n-1) y)
   
-head :: PositiveT s => Vector s a -> a
-head = P.head . unVec
+vhead :: PositiveT s => Vector s a -> a
+vhead = head . unVec
 
-tail :: PositiveT s => Vector s a -> Vector (Pred s) a
-tail = liftV P.tail
+vtail :: PositiveT s => Vector s a -> Vector (Pred s) a
+vtail = liftV tail
 
-last :: PositiveT s => Vector s a -> a
-last = P.last . unVec
+vlast :: PositiveT s => Vector s a -> a
+vlast = last . unVec
 
-init :: PositiveT s => Vector s a -> Vector (Pred s) a
-init = liftV P.init
+vinit :: PositiveT s => Vector s a -> Vector (Pred s) a
+vinit = liftV init
 
-take :: NaturalT i => i -> Vector s a -> Vector (Min s i) a
-take i = liftV $ P.take (fromIntegerT i)
+vtake :: NaturalT i => i -> Vector s a -> Vector (Min s i) a
+vtake i = liftV $ take (fromIntegerT i)
 
-drop :: NaturalT i => i -> Vector s a -> Vector (s :-: (Min s i)) a
-drop i = liftV $ P.drop (fromIntegerT i)
+vdrop :: NaturalT i => i -> Vector s a -> Vector (s :-: (Min s i)) a
+vdrop i = liftV $ drop (fromIntegerT i)
 
-select :: (NaturalT f, NaturalT s, NaturalT n, (f :<: i) ~ True, 
-          (((s :*: n) :+: f) :<=: i) ~ True) => 
-          f -> s -> n -> Vector i a -> Vector n a
-select f s n = liftV (select' f' s' n')
+vselect :: (NaturalT f, NaturalT s, NaturalT n, (f :<: i) ~ True, 
+           (((s :*: n) :+: f) :<=: i) ~ True) => 
+           f -> s -> n -> Vector i a -> Vector n a
+vselect f s n = liftV (select' f' s' n')
   where (f', s', n') = (fromIntegerT f, fromIntegerT s, fromIntegerT n)
-        select' f s n = ((selectFirst0 s n).(P.drop f))
+        select' f s n = ((selectFirst0 s n).(drop f))
         selectFirst0 :: Int -> Int -> [a] -> [a]
         selectFirst0 s n l@(x:_)
-          | n > 0 = x : selectFirst0 s (n-1) (P.drop s l)
+          | n > 0 = x : selectFirst0 s (n-1) (drop s l)
           | otherwise = []
         selectFirst0 _ 0 [] = []
 
 (<+) :: Vector s a -> a -> Vector (Succ s) a
-(<+) (Vector xs) x = Vector (xs P.++ [x])
+(<+) (Vector xs) x = Vector (xs ++ [x])
 
-(++) :: Vector s a -> Vector s2 a -> Vector (s :+: s2) a
-(++) = liftV2 (P.++)
+(<++>) :: Vector s a -> Vector s2 a -> Vector (s :+: s2) a
+(<++>) = liftV2 (++)
 
 infixl 5 <+
-infixr 5 ++
+infixr 5 <++>
 
-map :: (a -> b) -> Vector s a -> Vector s b
-map f = liftV (P.map f)
+vmap :: (a -> b) -> Vector s a -> Vector s b
+vmap f = liftV (map f)
 
-zipWith :: (a -> b -> c) -> Vector s a -> Vector s b -> Vector s c
-zipWith f = liftV2 (P.zipWith f)
+vzipWith :: (a -> b -> c) -> Vector s a -> Vector s' b -> Vector (Min s s') c
+vzipWith f = liftV2 (zipWith f)
 
-foldl :: (a -> b -> a) -> a -> Vector s b -> a
-foldl f e = (P.foldl f e) . unVec
+vfoldl :: (a -> b -> a) -> a -> Vector s b -> a
+vfoldl f e = (foldl f e) . unVec
 
-foldr :: (b -> a -> a) -> a -> Vector s b -> a
-foldr f e = (P.foldr f e) . unVec
+vfoldr :: (b -> a -> a) -> a -> Vector s b -> a
+vfoldr f e = (foldr f e) . unVec
 
-zip :: Vector s a -> Vector s b -> Vector s (a, b)
-zip = liftV2 P.zip
+vzip :: Vector s a -> Vector s b -> Vector s (a, b)
+vzip = liftV2 zip
 
-unzip :: Vector s (a, b) -> (Vector s a, Vector s b)
-unzip (Vector xs) = let (a,b) = P.unzip xs in (Vector a, Vector b)
+vunzip :: Vector s (a, b) -> (Vector s a, Vector s b)
+vunzip (Vector xs) = let (a,b) = unzip xs in (Vector a, Vector b)
 
-shiftIntoL :: (PositiveT s, NaturalT n, n ~ Pred s, s ~ Succ n) => 
+(+>>) :: (PositiveT s, NaturalT n, n ~ Pred s, s ~ Succ n) => 
+              a -> Vector s a -> Vector s a
+x +>> xs = x +> vinit xs
+
+(<<+) :: (PositiveT s, NaturalT n, n ~ Pred s, s ~ Succ n) => 
               Vector s a -> a -> Vector s a
-shiftIntoL xs x = x +> init xs
-
-shiftIntoR :: (PositiveT s, NaturalT n, n ~ Pred s, s ~ Succ n) => 
-              Vector s a -> a -> Vector s a
-shiftIntoR xs x = tail xs <+ x
+xs <<+ x = vtail xs <+ x
   
-rotl :: forall s a . NaturalT s => Vector s a -> Vector s a
-rotl = liftV rotl'
+vrotl :: forall s a . NaturalT s => Vector s a -> Vector s a
+vrotl = liftV rotl'
   where vlen = fromIntegerT (undefined :: s)
         rotl' [] = []
         rotl' xs = let (i,[l]) = splitAt (vlen - 1) xs
                    in l : i 
 
-rotr :: NaturalT s => Vector s a -> Vector s a
-rotr = liftV rotr'
+vrotr :: NaturalT s => Vector s a -> Vector s a
+vrotr = liftV rotr'
   where
     rotr' [] = []
-    rotr' (x:xs) = xs P.++ [x] 
+    rotr' (x:xs) = xs ++ [x] 
 
-concat :: Vector s1 (Vector s2 a) -> Vector (s1 :*: s2) a
-concat = liftV (P.foldr ((P.++).unVec) [])
+vconcat :: Vector s1 (Vector s2 a) -> Vector (s1 :*: s2) a
+vconcat = liftV (foldr ((++).unVec) [])
 
-reverse :: Vector s a -> Vector s a
-reverse = liftV P.reverse
+vreverse :: Vector s a -> Vector s a
+vreverse = liftV reverse
 
-iterate :: NaturalT s => (a -> a) -> a -> Vector s a
-iterate = iteraten (undefined :: s)
+viterate :: NaturalT s => (a -> a) -> a -> Vector s a
+viterate = viteraten (undefined :: s)
 
-iteraten :: NaturalT s => s -> (a -> a) -> a -> Vector s a
-iteraten s f x = let s' = fromIntegerT s in Vector (P.take s' $ P.iterate f x)
+viteraten :: NaturalT s => s -> (a -> a) -> a -> Vector s a
+viteraten s f x = let s' = fromIntegerT s in Vector (take s' $ iterate f x)
 
-generate :: NaturalT s => (a -> a) -> a -> Vector s a
-generate = generaten (undefined :: s)
+vgenerate :: NaturalT s => (a -> a) -> a -> Vector s a
+vgenerate = vgeneraten (undefined :: s)
 
-generaten :: NaturalT s => s -> (a -> a) -> a -> Vector s a
-generaten s f x = let s' = fromIntegerT s in Vector (P.take s' $ P.tail $ P.iterate f x)
+vgeneraten :: NaturalT s => s -> (a -> a) -> a -> Vector s a
+vgeneraten s f x = let s' = fromIntegerT s in Vector (take s' $ tail $ iterate f x)
 
-copy :: NaturalT s => a -> Vector s a
-copy x = copyn (undefined :: s) x
+vcopy :: NaturalT s => a -> Vector s a
+vcopy x = vcopyn (undefined :: s) x
 
-copyn :: NaturalT s => s -> a -> Vector s a
-copyn s x = iteraten s id x
+vcopyn :: NaturalT s => s -> a -> Vector s a
+vcopyn s x = viteraten s id x
 
-split :: ( NaturalT s
+vsplit :: ( NaturalT s
          -- , IsEven s ~ True
          ) => Vector s a -> (Vector (Div2 s) a, Vector (Div2 s) a)
-split (Vector xs) = (Vector (P.take vlen xs), Vector (P.drop vlen xs))
+vsplit (Vector xs) = (Vector (take vlen xs), Vector (drop vlen xs))
   where
-    vlen = round ((fromIntegral (P.length xs)) / 2)
+    vlen = round ((fromIntegral (length xs)) / 2)
 
 -- =============
 -- = Instances =
@@ -244,8 +238,8 @@ instance Show a => Show (Vector s a) where
 
 instance (Read a, NaturalT nT) => Read (Vector nT a) where
   readsPrec _ str
-    | all fitsLength possibilities = P.map toReadS possibilities
-    | otherwise = error (fName P.++ ": string/dynamic length mismatch")
+    | all fitsLength possibilities = map toReadS possibilities
+    | otherwise = error (fName ++ ": string/dynamic length mismatch")
     where 
       fName = "Data.Param.TFVec.read"
       expectedL = fromIntegerT (undefined :: nT)
@@ -254,10 +248,10 @@ instance (Read a, NaturalT nT) => Read (Vector nT a) where
       toReadS (xs, _, rest) = (Vector xs, rest)
       
 instance NaturalT s => DF.Foldable (Vector s) where
- foldr = foldr
+ foldr = vfoldr
  
 instance NaturalT s => Functor (Vector s) where
- fmap = map
+ fmap = vmap
 
 instance NaturalT s => DT.Traversable (Vector s) where 
   traverse f = (fmap Vector).(DT.traverse f).unVec
@@ -291,15 +285,15 @@ readVectorList :: Read a => String -> [([a], Int, String)]
 readVectorList = readParen' False (\r -> [pr | ("<",s) <- lexVector r,
                                               pr <- readl s])
   where
-    readl   s = [([],0,t) | (">",t) <- lexVector s] P.++
+    readl   s = [([],0,t) | (">",t) <- lexVector s] ++
                             [(x:xs,1+n,u) | (x,t)       <- reads s,
                                             (xs, n, u)  <- readl' t]
-    readl'  s = [([],0,t) | (">",t) <- lexVector s] P.++
+    readl'  s = [([],0,t) | (">",t) <- lexVector s] ++
                             [(x:xs,1+n,v) | (",",t)   <- lex s,
                                             (x,u)     <- reads t,
                                             (xs,n,v)  <- readl' u]
     readParen' b g  = if b then mandatory else optional
-      where optional r  = g r P.++ mandatory r
+      where optional r  = g r ++ mandatory r
             mandatory r = [(x,n,u) | ("(",s)  <- lexVector r,
                                       (x,n,t) <- optional s,
                                       (")",u) <- lexVector t]
